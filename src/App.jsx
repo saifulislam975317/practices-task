39b3cf14eb3e3a0e23e2772c93a01014c0bfa76b
saiful islam <saifulislam975317@gmail.com>
2023-10-27 19:05:12 +0600

    update info

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,40 @@ function App() {
         alias quia dolor reiciendis, obcaecati debitis quasi.
       </p>
       <Contact></Contact>
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt omnis
+        pariatur necessitatibus cum harum nihil laudantium minus, distinctio
+        commodi expedita ea, dolorum perspiciatis laboriosam molestias
+        aspernatur facilis magni qui quisquam iste rerum? Assumenda ea nam
+        neque, molestiae tempora maiores, accusantium dolorum fugiat voluptatum
+        voluptas velit libero error vitae, quod maxime ratione eligendi in natus
+        tenetur quibusdam mollitia. Consectetur rerum alias, omnis animi
+        delectus eius neque, at facere eos, vero eaque veritatis laboriosam
+        dolore distinctio quasi quod. Dolor possimus, doloribus impedit ad
+        libero odit blanditiis, unde, aspernatur mollitia dolorem sequi deserunt
+        fuga quae delectus in pariatur esse quasi repudiandae! Tenetur itaque
+        fuga distinctio ad perferendis alias impedit perspiciatis quas! Maxime
+        perferendis debitis repudiandae, placeat iusto sapiente voluptates odit
+        quas hic, temporibus repellat corrupti a iure. Illum eaque ratione
+        tempore eum deleniti ad eos. Sunt veritatis dignissimos quia at dolorum
+        id quam nulla minus impedit porro magnam ducimus repellendus fugit ipsa
+        ad ut excepturi quidem obcaecati tempora itaque nostrum, natus cumque.
+        Nostrum, numquam in obcaecati similique deleniti magnam recusandae saepe
+        repudiandae illo dolorem, ipsam autem vel ipsa quibusdam harum
+        voluptatibus aspernatur repellat fuga, nobis accusantium vitae atque.
+        Quibusdam molestias consequuntur quo aut voluptate provident animi, esse
+        ducimus sed libero reprehenderit labore, officia dolores, pariatur
+        minima quia. Recusandae quod debitis perspiciatis sit! Laudantium quod
+        sint modi amet totam hic non quia aut assumenda voluptatem in esse
+        consequuntur necessitatibus tempora ducimus consectetur nesciunt at, sed
+        fugiat illo. Aperiam corporis expedita vel non iste similique quasi eos
+        et deserunt, ipsam veniam ab a fugit. Iure dolorum alias in dignissimos
+        delectus corporis! Minus, voluptatum? Fugiat velit quaerat odio harum
+        excepturi sunt in perferendis numquam assumenda illo quisquam non nobis,
+        quos quae sed maxime atque eaque placeat ea magnam ex necessitatibus.
+        Dignissimos porro fuga nulla doloribus maxime laboriosam, ratione quas
+        facilis ullam ipsa sed dicta, illo nihil?
+      </h2>
     </>
   );
 }
